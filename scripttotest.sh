c4a62215a02fcea63b8ad95b1067d892ee979178
____marcell <0000marcell@gmail.com>
2023-07-20 18:02:11 +0000

    minor change

--- scripttotest.sh
+++ scripttotest.sh
@@ -13,10 +13,10 @@ cd $HOME
 # clone my configuration
 git clone git@github.com:0000marcell/nvim-ruby-dap-configuration.git
 # synlinc the configuration
-ln -sr nvim-ruby-dap-configuration/nvim/ ~/.config/
+ln -sr nvim-ruby-dap-configuration/nvim/ $HOME/.config/
 # install packer 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-	~/.local/share/nvim/site/pack/packer/start/packer.nvim
+	$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 # clone example code
 git clone git@github.com:0000marcell/nvim-ruby-dap-test.git
 # install example code dependencies
